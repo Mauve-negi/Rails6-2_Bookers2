f6e5e95ba72e0d4684aa92a90703ff0a15e5cfe1
Mauve-negi <116134417+Mauve-negi@users.noreply.github.com>
2023-04-02 16:13:37 +0000

    [Update]2023/04/03 1：13

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,17 @@
 class BooksController < ApplicationController
 
   def show
+    @user = User.find(params[:id])
     @book = Book.find(params[:id])
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
+    @user = current_user
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
@@ -41,6 +44,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title,:body)
   end
 end
